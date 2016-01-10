629ba1251f980359615e3af8ff10a2e30674c207
Liang Liu <nlprliu@gmail.com>
2016-01-10 11:59:44 -0500

    Added alias sdr1='screen -dR lliu1'

--- shortcuts.zsh
+++ shortcuts.zsh
@@ -1,3 +1,5 @@
-alias sdre='ssh ec2 -t "screen -dR"'
+alias sdr1='screen -dR lliu1'
+alias sdre2='ssh ec2 -t "screen -dR lliu2"'
+alias sdre='ssh ec2 -t "screen -dR lliu1"'
 alias ud2='diskutil unmountdisk /dev/disk2'
 alias wh='which'
