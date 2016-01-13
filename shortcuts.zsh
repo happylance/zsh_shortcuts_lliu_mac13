fbe5df736d9cfe290d4d9b3b3e9156c2c0c5c2cf
Liang Liu <nlprliu@gmail.com>
2016-01-12 22:36:23 -0500

    Added alias glgl='glg --author=Liang'

--- shortcuts.zsh
+++ shortcuts.zsh
@@ -1,4 +1,6 @@
+alias glgl='glg --author=Liang'
 alias sdr1='screen -dR lliu1'
+alias sdr2='screen -dR lliu2'
 alias sdre2='ssh ec2 -t "screen -dR lliu2"'
 alias sdre='ssh ec2 -t "screen -dR lliu1"'
 alias ud2='diskutil unmountdisk /dev/disk2'
