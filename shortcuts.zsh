46b1c14913b0b8db2f57a909f26c866f2449047c
Liang Liu <nlprliu@gmail.com>
2016-04-26 23:02:22 -0400

    Added alias setl='autossh ec2 -t  tmux attach -t lliu'

--- shortcuts.zsh
+++ shortcuts.zsh
@@ -12,14 +12,15 @@ alias sdr2='screen -dR lliu2'
 alias sdr='screen -dR lliu1'
 alias sdre2='ssh ec2 -t "screen -U -dR lliu2"'
 alias sdre='autossh -M 0 ec2 -t "screen -U -dR lliu1"'
+alias setl='autossh ec2 -t  tmux attach -t lliu'
 alias sew="ssh ec2 sudo tcpdump -U -s0 -w - 'not port 22' | wireshark -k -i -"
 alias shtc='autossh -M 0 htc'
 alias sr='$HOME/dev/scripts/reminder/showReminder.sh'
 alias sxe='autossh -M 0 -XC4c arcfour,blowfish-cbc ec2 -t "screen -U -dR lliu1"'
 alias wh='which'
-di() { open dict://$1 }
+di() { encoded=$(echo $@ | sed 's/ /%20/g'); open dict://$encoded; }
 dt() { dict -h dict.tw $1 | less }
 en() { [[ -d "$1" ]] && {cd "$1"; return}; [[ -f "$1" ]] && less $1 }
-go() { search=""; echo "Googling: $@"; for term in $@; do search="$search%20$term" done; open "http://www.google.com/search?q=$search" }
+go() { echo "Googling: $@"; search=$(echo $@ | sed 's/ /%20/g'); open "http://www.google.com/search?q=$search" }
 tcx() { touch "$1" && chmod +x "$1" }
 utm() { df -h | grep TimeMachine | awk '{printf $1}' | xargs diskutil unmountdisk }
